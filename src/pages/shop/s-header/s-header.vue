7d2618568ea988c671b489dc809ad7ff4f437821
jtr <jtr354@foxmail.com>
2018-10-10 14:58:11 +0800

    对接微信扫码

--- src/pages/shop/s-header/s-header.vue
+++ src/pages/shop/s-header/s-header.vue
@@ -43,6 +43,7 @@
         this.$router.push(this.$route.path + item.path)
       },
       _getScanner(callback) {
+        console.log(123)
         wx.scanQRCode({
           needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
