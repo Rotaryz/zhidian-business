93559c4da10942f249de43db29a684c5838cf8bf
jtr <jtr354@foxmail.com>
2018-12-10 17:12:31 +0800

    save code

---
+++ src/components/_base-wx-input/_base-wx-input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="base-wx-input">
+    base-wx-input
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'BASE_WX_INPUT'
+
+  export default {
+    name: COMPONENT_NAME,
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import '~common/stylus/mixin'
+
+  .base-wx-input
+    width: 100%
+</style>
