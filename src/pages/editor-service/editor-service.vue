2911ce6da3c73a082d0252a6dbee86d5d7569fa0
guojing <939316942@qq.com>
2018-12-27 10:07:23 +0800

    修改文字

--- src/pages/editor-service/editor-service.vue
+++ src/pages/editor-service/editor-service.vue
@@ -233,7 +233,7 @@
       this.type = this.$route.query.type
       this.id = this.$route.query.id
       if (this.id) {
-        document.title = '编辑活动'
+        document.title = '编辑服务'
         this._getServiceDetail(this.id)
       }
     },
