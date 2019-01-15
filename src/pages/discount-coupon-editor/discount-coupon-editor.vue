6ec26ea4fd0ac4e6bb9e2f65cecb32be8bf52a55
jtr <jtr354@foxmail.com>
2019-01-15 18:18:17 +0800

    调整input样式

--- src/pages/discount-coupon-editor/discount-coupon-editor.vue
+++ src/pages/discount-coupon-editor/discount-coupon-editor.vue
@@ -23,14 +23,17 @@
             </li>
             <li class="item-wrapper border-bottom-1px" @click="showPickerHandle('USE_TYPE_ARR')">
               <div class="left key-icon">优惠形式</div>
-              <div class="middle" :class="{'select-placeholder': disableEditor}">{{USE_TYPE_ARR}}</div>
+              <label class="middle" :class="{'select-placeholder': disableEditor}">
+                {{USE_TYPE_ARR}}
+                <input type="button" style="display: none">
+              </label>
               <base-icon-arrow></base-icon-arrow>
             </li>
             <li v-if="isShowDiscount" class="item-wrapper border-bottom-1px">
               <div class="left">折扣</div>
               <div class="middle">
                 <p v-if="disableEditor" class="select-placeholder">{{discounts}}</p>
-                <input v-else type="number" placeholder="请设置1.0~9.9之间的折扣额度" maxlength="3" v-model="discounts" @focus="blur()">
+                <input v-else type="number" placeholder="请设置1.0~9.9之间的折扣额度" maxlength="3" v-model="discounts">
               </div>
             </li>
             <li v-else class="item-wrapper border-bottom-1px">
@@ -229,21 +232,18 @@
       // 日期的 检查
       startReg() {
         let startDate = this.startDate.replace(/-/g, '/')
-        alert(startDate)
-        alert((new Date(startDate)).getTime())
         let start = (new Date(startDate)).getTime() + 1000 * 60 * 60 * 24
         if (!this.disableEditor) {
-          console.log(start)
-          alert(start)
-          alert(Date.now())
           return start >= Date.now()
         } else {
           return true
         }
       },
       dateReg() {
-        let end = (new Date('' + this.endDate)) * 1
-        let start = (new Date('' + this.startDate)) * 1
+        let startDate = this.startDate.replace(/-/g, '/')
+        let endDate = this.endDate.replace(/-/g, '/')
+        let end = (new Date(startDate)).getTime()
+        let start = (new Date(endDate)).getTime()
         return end > start
       }
     },
@@ -349,13 +349,13 @@
       // 检验数据有效性
       _checkForm(cb) {
         let arr = [
-          // {value: this.nameReg, txt: '请输优惠券名称'},
-          // {value: this.discountReg, txt: '请输入正确的优惠或折扣'},
-          // {value: this.minStockReg, txt: '发放数量不能少于' + this.minStock},
-          // {value: this.stockReg, txt: '请输入正确的发放数量'},
-          // {value: this.moneyLimitReg, txt: '请输入正确的门槛金额'},
-          // {value: this.selectItemReg, txt: '请选择商品或服务'},
-          {value: this.startReg, txt: '开始时间不能小于今天'},
+          {value: this.nameReg, txt: '请输优惠券名称'},
+          {value: this.discountReg, txt: '请输入正确的优惠或折扣'},
+          {value: this.minStockReg, txt: '发放数量不能少于' + this.minStock},
+          {value: this.stockReg, txt: '请输入正确的发放数量'},
+          {value: this.moneyLimitReg, txt: '请输入正确的门槛金额'},
+          {value: this.selectItemReg, txt: '请选择商品或服务'},
+          {value: this.startReg, txt: '开始日期不能小于今天'},
           {value: this.dateReg, txt: '结束日期不能小于开始日期'}
         ]
         let res = this._testPropety(arr)
@@ -437,6 +437,7 @@
         .left
           width :100px
         .middle
+          display :block
           flex:1
           overflow :hidden
           color: #27273E
