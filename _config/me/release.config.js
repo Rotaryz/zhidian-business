91360514c04a1ca1705c38ff4871635611f727a5
jtr <jtr354@foxmail.com>
2019-01-21 18:23:57 +0800

    ME域名配置

--- _config/me/release.config.js
+++ _config/me/release.config.js
@@ -1,8 +1,11 @@
 const version = ``
+const versionIm = ``
 export default {
   app: 'ME时代',
-  appKey: 'me',
   env: 'release',
   api: 'https://zhidian-me-era-api.jkweixin.net' + version,
-  upload: 'https://zhidian-me-era-api.jkweixin.net' + version
+  upload: 'https://zhidian-me-era-api.jkweixin.net' + version,
+  mall: 'https://exchange-mall-api.jkweixin.net' + version,
+  oauthURI: 'https://zhidian-me-era-api.jkweixin.net',
+  imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
